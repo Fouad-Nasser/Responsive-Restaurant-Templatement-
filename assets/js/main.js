77396b3bcbefb4136076562d6634d1af83859f20
Fouad <fouadnasser1998@gmail.com>
2022-04-12 21:53:00 +0200

    create order and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,3 +73,14 @@ var swiper = new Swiper(".popular-items", {
      },
   },
 });
+
+
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
